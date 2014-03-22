72a8de2ca22df1e4cd7b62c298046ab0fadc9f05
Vlada Boiko <vladislava.boiko@gmail.com>
2014-03-22 18:54:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Decorist::Application.config.secret_key_base = 'de526a29532f81765bb3bf96d748e0d7736d912f4c300019840e93385a9bc2eeb21e5fcfccab0cbd7010dd9a9d89f32e5b455a00a8fce9dbbd2ea3c545c127c6'
