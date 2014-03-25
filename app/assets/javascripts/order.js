15d5adc1729aeeaf50916e5f83651f79774e81e2
Vlada Boiko <vladislava.boiko@gmail.com>
2014-03-25 10:54:12 +0200

    enabled + button and style header

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
-            $('.minus').click(function () {
-                var $input = $(this).parent().find('input');
-                var count = parseInt($input.val()) - 1;
-                if(count === 0){
-                    $(this).closest('tr').remove();
-                }
-                $input.val(count);
-                $input.change();
-                return false;
-            });
-            $('.plus').click(function () {
-                var $input = $(this).parent().find('input');
-                $input.val(parseInt($input.val()) + 1);
-                $input.change();
-                return false;
-            });
-            $('.remove').click(function() {
-                $(this).closest('tr').remove();
-            });
-        });
+ $(document).ready(function() {
+  $('.minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      if(count === 0){
+          $(this).closest('tr').remove();
+      }
+      $input.val(count);
+      $input.change();
+      return false;
+  });
+  $('.plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      $('.sub').text($input.val() * parseFloat($('#price').text() + parseFloat($('.sub').text())).toFixed(2));
+      return false;
+  });
+  $('.remove').click(function() {
+      $(this).closest('tr').remove();
+  });
+});
